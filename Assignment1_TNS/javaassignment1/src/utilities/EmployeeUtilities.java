37cf8785c42b92e629a837859e1d75bc41c38f13
Mugilan K <123715281+Mugilan-krish@users.noreply.github.com>
2024-12-14 18:20:03 +0530

    Add files via upload

---
+++ Assignment1_TNS/javaassignment1/src/utilities/EmployeeUtilities.java
@@ -0,0 +1,34 @@
+package utilities;
+import employees.*;
+public class EmployeeUtilities {
+	
+
+	    /**
+	     * Displays basic information about an employee.
+	     * @param employee The Employee object.
+	     */
+	    public static void displayEmployeeInfo(Employee employee) {
+	        System.out.println("Name: " + employee.getName());
+	        System.out.println("Employee ID: " + employee.getEmployeeId());
+	        System.out.println("Salary: " + employee.getSalary());
+	    }
+
+	    /**
+	     * Updates the salary of an employee.
+	     * @param employee The Employee object.
+	     * @param newSalary The new salary to be set.
+	     */
+	    public static void updateEmployeeSalary(Employee employee, double newSalary) {
+	        employee.setSalary(newSalary);
+	        System.out.println("Updated Salary: " + employee.getSalary());
+	    }
+	}
+
+
+	
+	
+	
+	   
+	
+
+
